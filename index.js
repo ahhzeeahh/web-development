79b4552fe9bb60d5720d94cf8507e8234053eec0
Aziza O'Bryant <140858710+ahhzeeahh@users.noreply.github.com>
2024-03-11 15:23:08 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+window.onLoad = function() {
+    console.warn("Scripts have loaded..");
+    alert("Hello and welcome to my Web Devlopment Journey, this is still apart of azizaobryant.com")
+    
+}
