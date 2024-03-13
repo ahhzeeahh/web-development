be04b521386a3a757b8aea655e5c78b68840e4c4
aobryant <aziza.obryant@twdb.texas.gov>
2024-03-13 00:32:01 -0500

    end of day

--- odin-one/index.js
+++ odin-one/index.js
@@ -1,5 +1,11 @@
-window.onLoad = function() {
-    console.warn("Scripts have loaded..");
-    alert("Hello and welcome to my Web Devlopment Journey, this is still apart of azizaobryant.com")
+//alert("Hello and welcome to my Web Devlopment Journey, this is still apart of azizaobryant.com")
+    
+let btnGet = document.querySelector(".btn-dark")
+
+btnGet.addEventListener("click", colorChanger);
+
+function colorChanger() {
+    let randomNumber = Math.floor( Math.random()*100)
+    console.log(randomNumber)
     
 }
