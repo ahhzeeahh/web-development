900f1713e365a38f06c5a223a338d84caa1bd62b
azizaTWDB2 <aziza.obryant@twdb.texas.gov>
2024-03-18 14:07:55 -0500

    js updates

--- odin-one/index.js
+++ odin-one/index.js
@@ -1,5 +1,17 @@
 
-    let randomNumber = Math.floor( Math.random()*100)
-    console.log(randomNumber)
-    
+const btn = document.querySelector(".btn-dark");
+
+
+btn.addEventListener("click", function (){
+
+    const svg = document.querySelector(".bi-brilliance");
+
+    let rgb = "rgb(" + Math.floor( Math.random()*255) + "," + Math.floor( Math.random()*255) + "," + Math.floor( Math.random()*255) + ")"
+       
+        svg.style.color = rgb
+       
+        console.log(rgb)
+        
+
+})
 
