28c396a221f3c55fcf8cc0469f770b0aa42992c2
Aziza123 <azizaobryant@gmail.com>
2024-03-18 00:17:06 -0500

    mac updates

--- odin-one/index.js
+++ odin-one/index.js
@@ -1,11 +1,5 @@
-  
-let btnGet = document.querySelector(".btn-dark")
 
-btnGet.addEventListener("click", colorChanger);
-
-function colorChanger() {
     let randomNumber = Math.floor( Math.random()*100)
     console.log(randomNumber)
     
-}
 
