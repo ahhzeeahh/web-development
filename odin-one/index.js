6084c46e7d3c7dd17d2eb4ac5fb0035a1627a5de
Aziza123 <azizaobryant@gmail.com>
2024-06-05 16:36:32 -0500

    main merge

--- odin-one/index.js
+++ odin-one/index.js
@@ -1,21 +1,4 @@
 
-<<<<<<< HEAD
-
-const button = document.querySelector('#change');
-const icon = document.querySelector('.bi-brilliance');
-
-button.addEventListener('click', function() {
-
-    let number = 'rgb(' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ')';
-
-    icon.style.color = number;
-     
-
-    console.log(number)
-
-
-})
-=======
 const button = document.querySelector("#change");
 const icon = document.querySelector(".bi-brilliance");
 
@@ -28,4 +11,4 @@ button.addEventListener("click", function() {
       icon.style.color = rgb
 
   });
->>>>>>> test-branch
+
