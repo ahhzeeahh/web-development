73a834e81b329caab45812edbc6a07b729f5c57f
azizaTWDB <aziza.obryant@twdb.texas.gov>
2025-03-18 16:20:34 -0500

    .

--- odin-two/index.js
+++ odin-two/index.js
@@ -3,28 +3,27 @@ const submit = document.querySelector("#submit");
 const final = document.querySelector("#final");
 var input = document.querySelectorAll('input')
 var amount = 0;
+const formData = [];
 
-
-
+ let input1 = input[0].value
+    let input2 = input[1].value
+    let input3 = input[2].value
 
 
 submit.addEventListener('click', function() { 
 
-
+   
     
     var row = document.querySelectorAll('ul')
 
-        if (input[0].value == "" || input[1].value == "" ) {
+        if (input1 == "" || input2 == "" ) {
             alert("Please fill out required fields!")
             input[1].classList.add("error")
             input[0].classList.add("error")
         } else {
-            console.warn("Submitted: " + input[0].value  + " for $" + input[1].value  + " on " + input[2].value  + " ")
             input[1].classList.remove("error")
             input[0].classList.remove("error")
         }
-    
-  
         for (var i = 0; i < input.length; i++) {
 
             var list =  document.createElement('li');
@@ -36,12 +35,24 @@ submit.addEventListener('click', function() {
         input[1].value = amount;
         final.textContent = "$" + amount;
 
+        
+        const newObject = { 
+            title: input[0].value, 
+            money: input[1].value, 
+            date: input[2].value,
+            test: function() {
+                console.dir(this)
+
+            }
+
+        };
+
+        formData.push(newObject)
+
         input[0].value = "";
         input[1].value = "";
         input[2].value = "";
 });
 
-function StoredData(inputOne, inputTwo, inputThree) {
-   this.v
-}
 
+  
