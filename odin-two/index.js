77cea433a5b1b5b497f64bbf2c0201b509167b88
azizaTWDB <aziza.obryant@twdb.texas.gov>
2025-03-19 14:39:41 -0500

    redo form validation

--- odin-two/index.js
+++ odin-two/index.js
@@ -4,12 +4,89 @@ const final = document.querySelector("#final");
 var input = document.querySelectorAll('input')
 var amount = 0;
 const formData = [];
+let titleOf = input[0].value
+let amtOf = input[1].value
+let dateOf = input[2].value
 
- let input1 = input[0].value
-    let input2 = input[1].value
-    let input3 = input[2].value
+
+function getValues(titleOf, amtOf, dateOf) {
+    
+}
+
+/*
+function validateForm() {
 
 
+
+    if (titleOf == "" || amtOf == "") {
+        alert("Please fill out form")
+        input[0].classList.add("error");
+        input[1].classList.add("error");
+    } else{
+        input[0].classList.remove("error");
+        input[1].classList.remove("error");
+    }
+    alert("youclicked")
+     titleOf = ""
+     amtOf = "" 
+     dateOf = ""
+}
+*/
+
+
+
+function test() {
+    
+     
+    var row = document.querySelectorAll('ul')
+
+        if (input1 == "" || input2 == "" ) {
+            alert("Please fill out required fields!")
+            input[1].classList.add("error")
+            input[0].classList.add("error")
+        } else {
+            input[1].classList.remove("error")
+            input[0].classList.remove("error")
+        }
+        for (var i = 0; i < input.length; i++) {
+
+            var list =  document.createElement('li');
+               list.textContent = input[i].value
+               row[i].appendChild(list);        
+        }
+
+        amount +=  parseFloat(input[1].value)
+        input[1].value = amount;
+        final.textContent = "$" + amount;
+
+        
+        const newObject = { 
+            title: input[0].value, 
+            money: input[1].value, 
+            date: input[2].value,
+            test: function() {
+                console.dir(this)
+
+            }
+
+        };
+
+        formData.push(newObject)
+
+        input[0].value = "";
+        input[1].value = "";
+        input[2].value = "";
+}
+
+submit.addEventListener('click', validateForm())
+
+
+
+
+
+
+
+/*
 submit.addEventListener('click', function() { 
 
    
@@ -54,5 +131,5 @@ submit.addEventListener('click', function() {
         input[2].value = "";
 });
 
-
+*/
   
