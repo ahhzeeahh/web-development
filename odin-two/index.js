0ac236571f9ab43f184ad5b2798a59e14f4772d0
azizaTWDB <aziza.obryant@twdb.texas.gov>
2025-04-14 14:59:47 -0500

    code review 2

--- odin-two/index.js
+++ odin-two/index.js
@@ -36,15 +36,19 @@ function addToTable(e) {
 
     //doing this to practice with table methods and get way from innerHTML its still pretty longwinded
    let newRow = rowEl.insertRow(-1)
-   let cell1 = newRow.insertCell(0);    cell1.textContent = dataEntered.title
-   let cell2 = newRow.insertCell(1);    cell2.textContent = dataEntered.money
-   let cell3 = newRow.insertCell(2);    cell3.textContent = dataEntered.date
-   let cell4 = newRow.insertCell(3);    cell4.innerHTML = "<button>Delete?</button>";
+   let cell1 = newRow.insertCell(0);    
+   let cell4 = newRow.insertCell(3);  
+   let cell2 = newRow.insertCell(1);    
+   let cell3 = newRow.insertCell(2);    
+   cell1.textContent = dataEntered.title
+   cell2.textContent = dataEntered.money 
+   cell3.textContent = dataEntered.date  
+   cell4.innerHTML = "<button>Delete?</button>";
 
 
     var savedInt = Number(dataEntered.money)
 
-    //this stuff needs to be updated after every entry
+    //this stuff needs to be updated after every entry --LOOK ---form.reset()
    document.querySelectorAll("#my-form input").forEach(input => {input.value = '' });
 
    doMath(savedInt)
@@ -53,6 +57,7 @@ function addToTable(e) {
 }
 
 function doMath(savedInt) {
+// turn this into a 2 pram fuction with finial num and saved int take out DOM stuff... makbe make nef fucnigion to update total pass the reutned val from here
 
 //let allNumbers = document.querySelectorAll("#output-data tr td:nth-child(2)")
 console.log(savedInt + " is the number to be added/ delted bases on its positie or negitive status")
@@ -65,14 +70,16 @@ console.log(Number(finalNum.textContent) + " + " + savedInt)
     
 }
 
+// THIS NEEDS WORK... MAYBE only 1 submit event... 
 
 submitBtn.addEventListener('click', validateForm)
+//make a suunmit event that diviys up what to do from there aka call pther things
 formEl.addEventListener('submit', addToTable)
 savePrint.addEventListener('click', function () {
     window.print()
     JSON.stringify(dataEntered)
-
 })
+
 rowEl.addEventListener("click", function(e) {
     if (e.target.matches("td > button")) {
                 let btn = e.target
