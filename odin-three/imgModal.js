18b0d0b13f48c4ace919e09dfbb4d0b4678ac562
azizaTWDB <aziza.obryant@twdb.texas.gov>
2025-05-14 14:16:19 -0500

    break out into functions

--- odin-three/imgModal.js
+++ odin-three/imgModal.js
@@ -1,51 +1,68 @@
 /* For Image Gallery  */
 
 let imageListener = document.getElementById("txgioImgGall");
-let currImg = 1;
+let amountListener = document.querySelectorAll('[data-bs-img-num]')
+let currImgCounter = 0;
 let nextButtonImg = document.getElementById("show-next-image");
 let beforeButtonImg = document.getElementById("show-previous-image");
 let imgModalView = document.getElementById("imgGallModalView");
 let txgioImageGallery = document.querySelectorAll("#txgioImgGall img");
 
+nextButtonImg.addEventListener("click", nextImage)
+beforeButtonImg.addEventListener("click", beforeImage)
 imageListener.addEventListener("click", function(e) {
-    
-    if (e.target.matches("img.img")) {
+
+        validationButtons()
+        
+
+    if (e.target.matches("img.modal-img")) {
                 let img = e.target
+                console.log(img)
                 imgModalView.setAttribute("src", img.src)
-                let currImgSrc = img.closest('img.img')
-                let firstImg = img.closest('div')
-                currImg = parseInt(currImgSrc.getAttribute("data-bs-img-num"))
-
-                if (firstImg.classList.contains("txgio-img-gallery-1")) {
-                    beforeButtonImg.style.display = "none"
-                }else{
-                     beforeButtonImg.style.display = "block"
-                }
-                if (txgioImageGallery.length == currImg) {
-                    nextButtonImg.style.display = "none"
-                } else {
-                    nextButtonImg.style.display = "block"
-                }
-           
-
+                let currImgSrc = img.closest('img')
+                currImgCounter = parseInt(currImgSrc.getAttribute("data-bs-img-num"))
+        
+                return currImgCounter
     }    
 });
 
-beforeButtonImg.addEventListener("click", beforeImage)
+function validationButtons() {
+   console.log(currImgCounter)
+    if (currImgCounter = 1) {
+        beforeButtonImg.style.display = "block"
+    }else{
+         beforeButtonImg.style.display = "none"
+    }
+    if (txgioImageGallery.length = currImgCounter) {
+        nextButtonImg.style.display = "block"
+    } else {
+        nextButtonImg.style.display = "none"
+    }
 
-function beforeImage() {
-    let currImage = parseInt(imgModalView.getAttribute("data-bs-img-num")) - 1;
-    let newBeforeImg =  txgioImageGallery[currImage]
+    
+}
+
+function beforeImage(e) {
+
+    validationButtons()
+ 
+    let doMath = currImgCounter
+    let newBeforeImg =  txgioImageGallery[doMath]
     imgModalView.setAttribute("src", newBeforeImg.src)
+    currImgCounter = doMath
+    return currImgCounter
 
 }
 
-nextButtonImg.addEventListener("click", nextImage)
 
-function nextImage() {
-console.log("next was clickedS")
-    let newNextImg =  txgioImageGallery[currImg]
+
+function nextImage(e) {
+    validationButtons()
+
+    let doMath = currImgCounter
+    let newNextImg = txgioImageGallery[doMath]
     imgModalView.setAttribute("src", newNextImg.src)
-    currImg++
-    console.log(newNextImg)
+   currImgCounter = doMath
+   return currImgCounter
+
 }
