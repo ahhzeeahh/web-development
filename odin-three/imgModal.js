bdc2f9ff1713f950afe6f6e655fd7f90e0ed1d5e
azizaTWDB <aziza.obryant@twdb.texas.gov>
2025-07-10 09:25:58 -0500

    fixed

--- odin-three/imgModal.js
+++ odin-three/imgModal.js
@@ -1,50 +1,44 @@
-/* For Image Gallery  */
-
 let imageListener = document.getElementById("txgioImgGall");
-let amountListener = document.querySelectorAll('[data-bs-img-num]')
+let amountListener = document.querySelectorAll("[data-bs-img-num]");
 let currImgCounter = 0;
 let nextButtonImg = document.getElementById("show-next-image");
 let beforeButtonImg = document.getElementById("show-previous-image");
 let imgModalView = document.getElementById("imgGallModalView");
 let txgioImageGallery = document.querySelectorAll("#txgioImgGall img");
 
-nextButtonImg.addEventListener("click", clickerBtns)
-beforeButtonImg.addEventListener("click", clickerBtns)
-imageListener.addEventListener("click", function(e) {
-
-    if (e.target.matches("img.rounded")) {
-      
-                let img = e.target
-                currImgCounter = parseInt(img.getAttribute("data-bs-img-num")) - 1
-                imgModalView.setAttribute("src", img.src)
-                return currImgCounter
-    }    
-   
-});
-
-function clickerBtns(e){
-    let x = e.target
-    let test = x.closest('button')
+function clickerBtns(e) {
+    let x = e.target;
+    let test = x.closest("button");
 
     if (test.matches("#show-next-image")) {
-       
-        currImgCounter++
-        if(currImgCounter == txgioImageGallery.length - 1){
-            currImgCounter = 0
-            console.log(currImgCounter)
-            }
-    }else if (test.matches("#show-previous-image")) {
-        console.log(currImgCounter)
-        currImgCounter--
-        if(currImgCounter == -1){
-        currImgCounter = txgioImageGallery.length - 1
-    
+        currImgCounter++;
+        if (currImgCounter == txgioImageGallery.length - 1) {
+            currImgCounter = 0;
+            console.log(currImgCounter);
         }
-
+    } else if (test.matches("#show-previous-image")) {
+        console.log(currImgCounter);
+        currImgCounter--;
+        if (currImgCounter == -1) {
+            currImgCounter = txgioImageGallery.length - 1;
+        }
     }
 
-    let newNextImg = txgioImageGallery[currImgCounter]
-    imgModalView.setAttribute("src", newNextImg.src)
-    
-
+    let newNextImg = txgioImageGallery[currImgCounter];
+    imgModalView.setAttribute("src", newNextImg.src);
 }
+nextButtonImg.addEventListener("click", clickerBtns);
+beforeButtonImg.addEventListener("click", clickerBtns);
+imageListener.addEventListener("click", function (e) {
+    if (e.target.matches("img.rounded")) {
+        let img = e.target;
+        currImgCounter = parseInt(img.getAttribute("data-bs-img-num")) - 1;
+        imgModalView.setAttribute("src", img.src);
+        return currImgCounter;
+    }
+});
+document.getElementById("txgioImgGallModal").addEventListener('hide.bs.modal', () => {
+    if (document.activeElement instanceof HTMLElement) {
+        document.activeElement.blur();
+    }
+});
